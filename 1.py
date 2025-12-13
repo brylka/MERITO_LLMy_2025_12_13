452eef7f6be286ddbfaf807e96c6ebb33dd55aef
Bartosz Bryniarski <brylka@jelenia.com>
2025-12-13 09:48:46 +0100

    Sobota

--- 1.py
+++ 1.py
@@ -3,8 +3,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = genai.Client()
+history = []
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Cześć! Testuję!"
-)
-print(response.text)
+while True:
+    message = input("Ja: ")
+    history.append({"role": "user", "parts": [{"text": message}]})
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=history
+    )
+    history.append({"role": "model", "parts": [{"text": response.text}]})
+    print(f"Gemini: {response.text}")
